450b7f42e75ea5e26fc7c2a85a68c8609a0302c3
Gary Campbell <gary.campbell3690@gmail.com>
2023-04-26 12:56:32 +0100

    update

---
+++ js/poi.js
@@ -0,0 +1,31 @@
+var pois = [
+    {
+        name: "Cave Hill",
+        position: { x: 1, y: 3.5, z: 5 },
+        description: "For generations, Cave Hill has been synonymous with Belfast, with its imposing outline visible throughout the city. The landmark, named for the five caves located on the side of the cliffs, contains a wealth of natural, archaeological and historical features, including Belfast Castle. Its most famous feature, known locally as Napoleon's Nose, is believed to have been the inspiration for Jonathan Swift's novel, Gulliver's Travels."
+    },
+    {
+        name: "Belfast Castle",
+        position: { x: 1, y: 1.5, z: 5 },
+        description: "This is the description for POI 3"
+    },
+    {
+        name: "City Hall",
+        position: { x: -.1, y: 2, z: -5.2 },
+        description: "This is the description for POI 2"
+    },
+    {
+        name: "Samson & Goliath",
+        position: { x: -5, y: 3.5, z: 2 },
+        description: "This is the description for POI 3"
+    }
+    ,
+    {
+        name: "Titanic Exhibition",
+        position: { x: -5, y: 3.5, z: 1.8 },
+        description: "This is the description for POI 3",
+        lat: "54.60836082389292",
+        lng: "-5.91025433885457",
+    }
+];
+
