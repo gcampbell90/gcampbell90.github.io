410c130c653e21152ac4c4f398beee25e4aa5712
Gary Campbell <gary.campbell3690@gmail.com>
2023-04-26 15:33:15 +0100

    latlongs added

--- js/poi.js
+++ js/poi.js
@@ -2,22 +2,30 @@ var pois = [
     {
         name: "Cave Hill",
         position: { x: 1, y: 3.5, z: 5 },
-        description: "For generations, Cave Hill has been synonymous with Belfast, with its imposing outline visible throughout the city. The landmark, named for the five caves located on the side of the cliffs, contains a wealth of natural, archaeological and historical features, including Belfast Castle. Its most famous feature, known locally as Napoleon's Nose, is believed to have been the inspiration for Jonathan Swift's novel, Gulliver's Travels."
+        description: "For generations, Cave Hill has been synonymous with Belfast, with its imposing outline visible throughout the city. The landmark, named for the five caves located on the side of the cliffs, contains a wealth of natural, archaeological and historical features, including Belfast Castle. Its most famous feature, known locally as Napoleon's Nose, is believed to have been the inspiration for Jonathan Swift's novel, Gulliver's Travels.",
+        lat: "54.60836082389292",
+        lng: "-5.91025433885457",
     },
     {
         name: "Belfast Castle",
         position: { x: 1, y: 1.5, z: 5 },
-        description: "This is the description for POI 3"
+        description: "This is the description for POI 3",
+        lat: "54.60836082389292",
+        lng: "-5.91025433885457",
     },
     {
         name: "City Hall",
         position: { x: -.1, y: 2, z: -5.2 },
-        description: "This is the description for POI 2"
+        description: "This is the description for POI 2",
+        lat: "54.60836082389292",
+        lng: "-5.91025433885457",
     },
     {
         name: "Samson & Goliath",
         position: { x: -5, y: 3.5, z: 2 },
-        description: "This is the description for POI 3"
+        description: "This is the description for POI 3",
+        lat: "54.60836082389292",
+        lng: "-5.91025433885457",
     }
     ,
     {
